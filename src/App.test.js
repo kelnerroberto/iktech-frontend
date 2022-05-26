3851b612f30ff39cc8fc2f5aef7c502dde9f63b6
Kelner Roberto <kelner250@hotmail.com>
2022-05-26 16:11:14 -0300

    do another test to see if there's 2 inputs on the document

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { render } from './test-utils';
 import App from './App';
 
-test('renders learn react link', () => {
+test("Renders button element with text 'Add commitment'", () => {
   render(<App />);
   const linkElement = screen.getByText(/Add commitment/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('Check if there are 2 input fields', () => {
+  render(<App />);
+  const inputName = screen.getByPlaceholderText('Nome');
+  const inputTitle = screen.getByPlaceholderText('Título');
+  expect(inputName).toBeInTheDocument();
+  expect(inputTitle).toBeInTheDocument();
 });
